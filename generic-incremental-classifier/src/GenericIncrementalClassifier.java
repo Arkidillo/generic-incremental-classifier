82303f1a164d632184bb01e212cb5bd812bfb7d4
loaner <arkidillo@gmail.com>
2018-01-29 18:26:20 -0500

    Add initial java and py files

---
+++ generic-incremental-classifier/src/GenericIncrementalClassifier.java
@@ -0,0 +1,10 @@
+public class GenericIncrementalClassifier {
+    public static void main (String [] args) {
+        // Start window
+        // Run user-input step
+        // Loop:
+            // Train
+            // Test image
+            // User correction
+    }
+}
