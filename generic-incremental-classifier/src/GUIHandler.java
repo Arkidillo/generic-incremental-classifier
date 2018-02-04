f012303ec5db9fe678ffe4198d491741ceb61e41
loaner <arkidillo@gmail.com>
2018-02-04 13:42:57 -0500

    Add GUIHandler class

---
+++ generic-incremental-classifier/src/GUIHandler.java
@@ -0,0 +1,5 @@
+public class GUIHandler {
+    public void createWindow() {
+
+    }
+}
--- generic-incremental-classifier/src/GenericIncrementalClassifier.java
+++ generic-incremental-classifier/src/GenericIncrementalClassifier.java
@@ -1,6 +1,9 @@
 public class GenericIncrementalClassifier {
     public static void main (String [] args) {
         // Start window
+        GUIHandler gui = new GUIHandler();
+        gui.createWindow();
+
         // Run user-input step
         // Loop:
             // Train
